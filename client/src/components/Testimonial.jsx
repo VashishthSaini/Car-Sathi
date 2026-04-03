662e1e4da40754f44530c7442d40086f52ac968f
Vashishth Saini <vashishthsaini.tech@gmail.com>
2026-04-03 08:40:11 +0530

    Update Testimonial.jsx

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Title from ".owner/Title";
+import Title from ".components/owner/Title";
 import { assets } from "../assets/assets";
 import { motion } from "motion/react";
 
